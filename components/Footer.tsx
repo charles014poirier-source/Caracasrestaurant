7e7ffc37887fb839064680aa2254239f4754b1ae
charles014poirier-source <charles014poirier@gmail.com>
2026-03-28 02:09:04 +0100

    feat: update Footer component to pass client data

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,10 @@
 import { Footer7 } from "@/components/ui/footer-7";
+import { caracasConfig } from '@/data/clients/caracas';
 
 export function Footer() {
-  return <Footer7 />;
+  return <Footer7
+    contactInfo={caracasConfig.contact}
+    config={caracasConfig.config}
+    hours={caracasConfig.hours}
+  />;
 }
